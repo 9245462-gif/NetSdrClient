68989bad1f93164ef3c43d7e47849e0bcbd154e2
9245462-gif <9245462@stud.kai.edu.ua>
2025-11-20 22:36:15 +0200

    Failed arch test

--- NetSdrClientApp/Messages/NetSdrMessageHelper.cs
+++ NetSdrClientApp/Messages/NetSdrMessageHelper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using NetSdrClientApp.Networking; // Навмисне порушення залежності
+
 
 namespace NetSdrClientApp.Messages
 {
@@ -16,6 +18,13 @@ namespace NetSdrClientApp.Messages
         private const short _msgControlItemLength = 2; //2 byte, 16 bit
         private const short _msgSequenceNumberLength = 2; //2 byte, 16 bit
 
+        // Додайте будь-яке посилання на Networking для порушення
+        private static void SomeMethod()
+        {
+            // Це порушить тест Messages_Should_Not_Depend_On_Networking
+            var dummy = typeof(ITcpClient).Name;
+        }
+        
         public enum MsgTypes
         {
             SetControlItem,
